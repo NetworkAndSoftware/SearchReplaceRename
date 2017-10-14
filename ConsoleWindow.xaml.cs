71af9da62c8b31aed5b1cdb9b03b2d3de09aa35e
windsunrain <windsunrain@gmail.com>
2017-10-13 21:56:11 -0700

    Add project files.

---
+++ ConsoleWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace SearchReplaceRename
+{
+  /// <summary>
+  ///   Interaction logic for ConsoleWindow.xaml
+  /// </summary>
+  public partial class ConsoleWindow : Window
+  {
+    public ConsoleWindow(string path, string search, string replace)
+    {
+      Path = path;
+      Search = search;
+      Replace = replace;
+
+      InitializeComponent();
+    }
+
+    public string Path { get; set; }
+    public string Search { get; set; }
+    public string Replace { get; set; }
+
+    private void Window_Initialized(object sender, EventArgs e)
+    {
+      var worker = new BackgroundWorker();
+      worker.DoWork += DoWork;
+      worker.RunWorkerCompleted += RunWorkerCompleted;
+      worker.RunWorkerAsync();
+    }
+
+    private void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs args)
+    {
+      btnDismiss.IsEnabled = true;
+    }
+
+    private void DoWork(object sender, DoWorkEventArgs args)
+    {
+      if (!Directory.Exists(Path))
+      {
+        WriteLine("ERROR: Path does not exist. Please select a valid path and try again");
+      }
+      else
+      {
+        WriteLine(
+          string.Format(@"Renaming all files and directories containing '{0}' to have '{1}' instead in path '{2}'",
+            Search, Replace, Path));
+        SearchAndReplaceRename(Search, Replace, Path);
+      }
+    }
+
+    private void WriteLineSync(string line)
+    {
+      textBox1.Text += "\n" + line;
+    }
+
+    private void WriteLine(string line)
+    {
+      Dispatcher.Invoke(DispatcherPriority.Normal, new Action<string>(WriteLineSync), line);
+    }
+
+    private void SearchAndReplaceRename(string search, string replace, string path)
+    {
+      foreach (var file in Directory.GetFiles(path))
+        RenameIfMatch(file, search, replace);
+
+      foreach (var directory in Directory.GetDirectories(path))
+      {
+        SearchAndReplaceRename(search, replace, directory);
+        RenameIfMatch(directory, search, replace);
+      }
+    }
+
+    private static string LastPathSegment(string path)
+    {
+      var parts = path.Split('\\');
+      return parts.Length > 0 ? parts[parts.Length - 1] : path;
+    }
+
+    private static string ExtendPath(string path, string name)
+    {
+      const string FORMAT = @"{0}\{1}";
+      return string.Format(FORMAT, path, name);
+    }
+
+    private void RenameIfMatch(string path, string search, string replace)
+    {
+      var index = path.LastIndexOf('\\');
+      var pathbase = index == -1 ? string.Empty : path.Substring(0, index);
+      var last = path.Substring(index + 1);
+
+      if (last.Contains(search))
+      {
+        var oldname = ExtendPath(pathbase, last);
+        var newname = ExtendPath(pathbase, last.Replace(search, replace));
+
+        WriteLine(string.Format(@"Renaming '{0}' to '{1}'", oldname, newname));
+
+        try
+        {
+          Directory.Move(oldname, newname);
+        }
+        catch (Exception x)
+        {
+          WriteLine(string.Format(@"Exception: {0}", x.Message));
+        }
+      }
+    }
+
+    private void btnDismiss_Click(object sender, RoutedEventArgs e)
+    {
+      Close();
+    }
+  }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
+
+namespace SearchReplaceRename
+{
+  /// <summary>
+  ///   Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void btnChooseFile_Click(object sender, RoutedEventArgs e)
+    {
+      var dialog = new FolderBrowserDialog();
+
+      if (System.Windows.Forms.DialogResult.OK == dialog.ShowDialog())
+        tbPath.Text = dialog.SelectedPath;
+    }
+
+    private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+    {
+      CheckIfReadyToGo();
+    }
+
+    private void tbReplace_TextChanged(object sender, TextChangedEventArgs e)
+    {
+      CheckIfReadyToGo();
+    }
+
+    private void tbPath_TextChanged(object sender, TextChangedEventArgs e)
+    {
+      CheckIfReadyToGo();
+    }
+
+    private void CheckIfReadyToGo()
+    {
+      btnGo.IsEnabled = 0 != tbSearch.Text.Length && 0 != tbReplace.Text.Length && 0 != tbPath.Text.Length;
+    }
+
+    private void btnGo_Click(object sender, RoutedEventArgs e)
+    {
+      if (MessageBoxResult.Yes ==
+          MessageBox.Show(
+            "Please be aware that what you're about to do is potentially destructive and irreversible. If you know what you're doing, click the Yes button. If not, please click the No button",
+            "Danger, Will Robinson!", MessageBoxButton.YesNo))
+      {
+        var dialog = new ConsoleWindow(tbPath.Text, tbSearch.Text, tbReplace.Text);
+        dialog.ShowDialog();
+      }
+    }
+  }
+}
